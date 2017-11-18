72efcf76d12d805ef823cf6b42ba9c3e768bc841
vagrant <vagrant@homestead>
2017-11-18 15:35:38 +0000

    完成侧边栏样式

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,16 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
-window.Vue = require('vue');
+const logoutUrl = '/logout';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+$('.logout').on('click', function () {
+    axios.post(logoutUrl)
+        .then(function () {
+            window.location.reload();
+        })
+});
 
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
+$('.sidebar-item').on('click', function () {
+    href = $(this).attr('href');
+    if (!isEmpty(url))
+        window.location.href = href;
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.console')
 
 @section('content')
-<div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
@@ -19,5 +18,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
